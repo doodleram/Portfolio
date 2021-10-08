2b23e25e935a088b1e3e50704f5c0789aed88ac3
yeram <dmgk231@naver.com>
2021-10-08 15:41:14 +0900

    211008-1

--- js/script.js
+++ js/script.js
@@ -1,7 +1,48 @@
-$(function(){
+$(document).ready(function(){
 
-    $(".webstandard").click(function(){
+    $(".webstandard").click(function(e){
+        e.preventDefault();
         $(".w_modal").show();
     });
+    $(".quit").click(function(){
+        $(".w_modal").hide();
+    });
+    //w_modal
+
+    $(".overflow").click(function(e){
+        e.preventDefault();
+        $(".o_modal").show();
+    });
+    $(".quit").click(function(){
+        $(".o_modal").hide();
+    });
+    //o_modal
+
+    $(".pilates").click(function(e){
+        e.preventDefault();
+        $(".p_modal").show();
+    });
+    $(".quit").click(function(){
+        $(".p_modal").hide();
+    });
+    //p_modal
+
+    $(".applemusic").click(function(e){
+        e.preventDefault();
+        $(".a_modal").show();
+    });
+    $(".quit").click(function(){
+        $(".a_modal").hide();
+    });
+    //a_modal
+
+    $(".parallax").click(function(e){
+        e.preventDefault();
+        $(".x_modal").show();
+    });
+    $(".quit").click(function(){
+        $(".x_modal").hide();
+    });
+    //x_modal
 
 });
